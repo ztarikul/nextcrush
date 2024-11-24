6af5b6ee3da4e0e45555c722ea235db65a67cd2d
ztarikul <ztarikulislam@gmail.com>
2024-11-24 10:13:23 +0600

    Promise fetching parallelly

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import getPost from "../../../lib/getPost";
-import { title } from "process";
+import getPostComments from "../../../lib/getPostComment";
 
 export async function generateMetadata(props) {
   const { id } = props.params;
@@ -15,12 +15,31 @@ export async function generateMetadata(props) {
 export default async function PostPage(props) {
   const { id } = props.params;
 
-  const post = await getPost(id);
+  // const post = await getPost(id);
+  // const comments = await getPostComments(id);
+
+  // Await: always wait for previous function or line of code execute, But we need to execute parallelly
+
+  const postPromise = getPost(id);
+  const commentsPromise = getPostComments(id);
+
+  const [post, comments] = await Promise.all([postPromise, commentsPromise]);
 
   return (
     <div className="mt-6">
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
+      <hr />
+      <div className="mt-6">
+        <h1>Comments</h1>
+        <ul>
+          {comments.map((comment) => (
+            <li className="mb-2" key={comment.id}>
+              {comment.body}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
